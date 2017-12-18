cb92e26f61ddb4d4b17ac8b61a4d2c0594793b13
cogito96 <bujnolukasz@gmail.com>
2017-12-18 13:40:50 +0100

    gotowy layout

---
+++ app/src/main/java/com/example/aa/recyclerviewdemo/DetailActivity.java
@@ -0,0 +1,10 @@
+package com.example.aa.recyclerviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by aa on 18.12.2017.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+}
