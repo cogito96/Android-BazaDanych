7dc9054b5005c9c9c73961ad80cad811d408255b
cogito96 <bujnolukasz@gmail.com>
2017-12-18 20:14:40 +0100

    zrobinie programu na sztucznych danych

--- app/src/main/java/com/example/aa/recyclerviewdemo/view/ListActivity.java
+++ app/src/main/java/com/example/aa/recyclerviewdemo/view/ListActivity.java
@@ -1,15 +1,124 @@
 package com.example.aa.recyclerviewdemo.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.aa.recyclerviewdemo.R;
+import com.example.aa.recyclerviewdemo.data.FakeDataSource;
+import com.example.aa.recyclerviewdemo.data.ListItem;
+import com.example.aa.recyclerviewdemo.logic.Controller;
 
-public class ListActivity extends AppCompatActivity {
+import java.util.List;
+
+public class ListActivity extends AppCompatActivity implements ViewInterface{
+
+    private static final String EXTRA_DATE_AND_TIME ="EXTRA_DATE_AND_TIME";
+    private static final String EXTRA_MESSAGE ="EXTRA_MESSAGE";
+    private static final String EXTRA_COLOUR ="EXTRA_COLOUR";
+
+    private List<ListItem> listOfData;
+
+    private LayoutInflater layoutInflater;
+    private RecyclerView recyclerView;
+    private CustomAdapter adapter;
+
+    private Controller controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+         setContentView(R.layout.activity_list);
+
+        recyclerView = (RecyclerView) findViewById(R.id.rec_list_activity);
+        layoutInflater = getLayoutInflater();
+
+        controller = new Controller(this, new FakeDataSource());
+    }
+
+    @Override
+    public void startDetailActivity(String dateAndTime, String message, int colorResource) {
+        Intent i = new Intent(this,DetailActivity.class);
+        i.putExtra(EXTRA_DATE_AND_TIME,dateAndTime);
+        i.putExtra(EXTRA_MESSAGE,message);
+        i.putExtra(EXTRA_COLOUR,colorResource);
+
+        startActivity(i);
+    }
+
+    @Override
+    public void setUpAdapterAndView(List<ListItem> listOfData) {
+        this.listOfData = listOfData;
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new CustomAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    private class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder>{
+        @Override
+        public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = layoutInflater.inflate(R.layout.item_data,parent,false);
+            return new CustomViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(CustomViewHolder holder, int position) {
+            ListItem currentItem = listOfData.get(position);
+
+            holder.coloredCircle.setBackgroundResource(
+                    currentItem.getColorResource()
+            );
+
+            holder.message.setText(
+                    currentItem.getMessage()
+            );
+
+            holder.dateAndTime.setText(
+                    currentItem.getDateAndTime()
+            );
+        }
+
+        @Override
+        public int getItemCount() {
+            return listOfData.size();
+        }
+
+        class CustomViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+            private View coloredCircle;
+            private TextView dateAndTime;
+            private TextView message;
+            private ViewGroup container;
+
+            public CustomViewHolder(View itemView) {
+                super(itemView);
+
+                this.coloredCircle = itemView.findViewById(R.id.imv_list_item_circle);
+                this.dateAndTime = (TextView) itemView.findViewById(R.id.lbl_date_and_time);
+                this.message = (TextView) itemView.findViewById(R.id.lbl_message);
+                this.container = (ViewGroup) itemView.findViewById(R.id.root_list_item);
+
+                this.container.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View v) {
+                ListItem listItem = listOfData.get(
+                        this.getAdapterPosition()
+                );
+
+                controller.onListItemClick(
+                        listItem
+                 );
+            }
+        }
     }
 }
